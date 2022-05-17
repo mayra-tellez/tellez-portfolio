c5d1181e46201fe1ac4b74d4acc2c5f19b3042c8
mayrant <mayrant2004@yahoo.com>
2022-05-17 16:03:59 -0500

    portfolio and project card start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import ProjectCard from './Components/ProjectCard';
 import './App.css';
 
 function App() {
+
+  const timeOfDay = () => {
+    const today = new Date();
+    const time = today.getHours();
+  
+    if (time >= 0 && time < 12) {
+      return "morning";
+    } else if (time >= 12 && time < 17) {
+      return "afternoon";
+    } else if (time >= 17 && time <= 23) {
+      return "evening";
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <p>Mayra Téllez</p>
+        <p>Based in Austin, TX</p>
+        <p>Currently a Software Developer at Zollege</p>
+        <nav>
+          <a href="">About</a>
+          <a href="">Work</a>
+          <a href="">Resume</a>
+        </nav>
       </header>
+      <main>
+        <h3>Thanks for stopping by!</h3>
+        <a href="">View my work</a>
+      </main>
+      <footer>
+        <p>Enjoy your {timeOfDay()}!</p>
+        <p>Get in touch.</p>
+        <div>Home page toggle.</div>
+      </footer>
+      <ProjectCard></ProjectCard>
     </div>
   );
 }
---
+++ src/Components/ProjectCard.js
@@ -0,0 +1,31 @@
+import React from "react";
+import projects from "../projects.json";
+import "./ProjectCard.css";
+
+const ProjectCard = () => {
+  const projectList = projects.projects;
+
+  return (
+    <div className="projectCardContainer">
+      {projectList.map((project, i) => {
+        const { name, code_url, live_url, tags, description, image } = project;
+        return (
+          <div className="projectCard" key={i}>
+            <h3>{name}</h3>
+            <a href={code_url} target="blank">Code Repo</a>
+            <a href={live_url} target="blank">Live Site</a>
+            <p>{description}</p>
+            <ul>
+              {tags.map((tag, i) => {
+                return <li key={i}>{tag}</li>
+              })}
+            </ul>
+            <img alt="project thumbnail" src={image}></img>
+          </div>
+        )
+      })}
+    </div>
+  );
+}
+
+export default ProjectCard;
