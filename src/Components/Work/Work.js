3b5af0bcd13f6d3b76a72e05d6e36a53e9ae103c
mayrant <mayrant2004@yahoo.com>
2022-08-01 16:18:45 -0500

    progress on work and project card components, needs dynamic rendering on project image

--- src/Components/Work/Work.js
+++ src/Components/Work/Work.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import projects from "../../projects.json";
@@ -9,12 +10,67 @@ import image from "../../images/landingPageThumbnail.png";
 const Work = () => {
   const projectsList = projects.projects;
 
+  let projectCards = document.getElementsByClassName("ProjectCard");
+
+  const mouseOver = () => {
+    // document.getElementById("home").style.color = "red";
+    console.log("moused! eek");
+  }
+
+  // const mouseOut = () => {
+  //   console.log("OUT");
+  // }
+
+  useEffect(() => {
+    // document.getElementById("home").addEventListener("mouseout", mouseOut);
+    // Array.from(projectCards).forEach(card => {
+    //   console.log("HERE YE", card);
+    //   card.addEventListener("mouseover", mouseOver);
+    // })    
+
+    let projectCardsArray = Array.from(projectCards);
+    console.log(projectCardsArray);
+    
+    Array.from(projectCards).forEach(card => {
+      console.log("HERE YE", card);
+      card.addEventListener("mouseover", mouseOver);
+    })
+
+    // Array.from(projectCards).forEach(card => {
+    //   card.addEventListener("mouseout", mouseOut);
+    // })
+  })
+
+  // projectCardsArray.map(projectCard => {
+  //   projectCard.addEventListener("mouseover", mouseover())
+  //   return true;
+  // })
+
+  const projectInfo = projectsList.map(project => {
+    return project.name;
+  })
+
+  console.log("project list json data:", projectInfo);
+  
+  // const projectCards = document.getElementsByClassName("ProjectCard");
+  // console.log("project cards list", projectCards);
+
+  // const projectCardsArray = [ ...projectCards ];
+  // console.log("project array", projectCardsArray);
+
+  // projectCards.map(card => {
+  //   console.log("mouseover", card)
+  //   return true;
+  // })
+
   return (
     <div className="Work">
       <section className="titleAndImage-section">
-        <Link to="/">back to home</Link>
+        <Link className="navToHome-link" to="/">back to <span id="home">home</span></Link>
+        <div className="project-image-container">
+          {/* <img className="project-image" src={image} alt="project thumbnail" /> */}
+        </div>
         <h1 className="page-title">work</h1>
-        <img className="project-image" src={image} alt="project thumbnail" />
       </section>
       <section className="projects-section">
         {projectsList.map((project, i) => {
