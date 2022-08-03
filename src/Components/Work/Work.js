567640c2b21cc7b1e08e7c5063dca1ef123ce6f7
mayrant <mayrant2004@yahoo.com>
2022-08-02 19:53:43 -0500

    dynamic rendering of project image working

--- src/Components/Work/Work.js
+++ src/Components/Work/Work.js
@@ -1,78 +1,52 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import projects from "../../projects.json";
+import ProjectCard from "../Projects/ProjectCard";
 import "../../index.css";
 import "./Work.css";
-import ProjectCard from "../Projects/ProjectCard";
-import image from "../../images/landingPageThumbnail.png";
 
 const Work = () => {
   const projectsList = projects.projects;
 
+  const [imageDisplay, setImageDisplay] = useState(false);
+  const [imageData, setImageData] = useState({});
+
+  const displayImagePath = imageData.projectImagePath;
+  const displayImageAlt = imageData.projectImageAlt;
+
   let projectCards = document.getElementsByClassName("ProjectCard");
 
-  const mouseOver = () => {
-    // document.getElementById("home").style.color = "red";
-    console.log("moused! eek");
-  }
-
-  // const mouseOut = () => {
-  //   console.log("OUT");
-  // }
-
   useEffect(() => {
-    // document.getElementById("home").addEventListener("mouseout", mouseOut);
-    // Array.from(projectCards).forEach(card => {
-    //   console.log("HERE YE", card);
-    //   card.addEventListener("mouseover", mouseOver);
-    // })    
 
-    let projectCardsArray = Array.from(projectCards);
-    console.log(projectCardsArray);
-    
     Array.from(projectCards).forEach(card => {
-      console.log("HERE YE", card.dataset);
-      card.addEventListener("mouseover", mouseOver);
+
+      card.addEventListener("mouseover", () => {
+        setImageDisplay(true);
+        setImageData(card.dataset);
+      });
+
+      card.addEventListener("mouseleave", () => {
+        setImageDisplay(false);
+        setImageData({});
+      });
     })
-
-    // Array.from(projectCards).forEach(card => {
-    //   card.addEventListener("mouseout", mouseOut);
-    // })
   })
-
-  // projectCardsArray.map(projectCard => {
-  //   projectCard.addEventListener("mouseover", mouseover())
-  //   return true;
-  // })
-
-  const projectInfo = projectsList.map(project => {
-    return project.name;
-  })
-
-  console.log("project list json data:", projectInfo);
-  
-  // const projectCards = document.getElementsByClassName("ProjectCard");
-  // console.log("project cards list", projectCards);
-
-  // const projectCardsArray = [ ...projectCards ];
-  // console.log("project array", projectCardsArray);
-
-  // projectCards.map(card => {
-  //   console.log("mouseover", card)
-  //   return true;
-  // })
 
   return (
     <div className="Work">
       <section className="titleAndImage-section">
         <Link className="navToHome-link" to="/">back to <span id="home">home</span></Link>
         <div className="project-image-container">
-          {/* <img className="project-image" src={image} alt="project thumbnail" /> */}
+          {
+            imageDisplay ? 
+            <img id="project-image" alt={displayImageAlt} src={require("../../images/" + displayImagePath + ".png")}></img> :
+            null
+          }
         </div>
         <h1 className="page-title">work</h1>
       </section>
-      <section className="projects-section">
+      <section id="projects-section">
         {projectsList.map((project, i) => {
           return (
             <ProjectCard project={project} key={i}></ProjectCard>
