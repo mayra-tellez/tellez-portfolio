1536bdfd0609b6b876818f850da991bb2e1efbcb
mayrant <mayrant2004@yahoo.com>
2022-08-04 19:04:26 -0500

    redesigned contact component

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -7,32 +7,37 @@ const Contact = () => {
   return (
     <div className="Contact">
       <Link className="navToHome-link" to="/">back to <span id="home">home</span></Link>
-      <section className="contact-main">
-        <h1 className="page-title">contact</h1>
-        <div className="info-container info-container-one">
-          <p className="contact-method">Email</p>
+      <h1 className="page-title">contact</h1>
+
+      <div className="info-container info-container-one">
+        <p className="contact-method">Email</p>
+        <div className="email-container">
           <div className="email">mayranicoletellez@gmail.com</div>
         </div>
-        <div className="info-container info-container-two">
-          <p className="contact-method">Social</p> 
-          <div className="socials-container">
-            <span className="social-with-arrow">
-              <a className="social" href="https://www.linkedin.com/in/mayra-tellez/" target="blank">
-                LinkedIn
-              </a>
-              <span className="arrow">&#8599;</span>
-            </span>
-            <span className="social-with-arrow">
-              <a className="social" href="https://github.com/mayra-tellez" target="blank">Github</a>            
-              <span className="arrow">&#8599;</span>
-            </span>
-          </div>
+      </div>
+
+      <div className="info-container info-container-two">
+        <p className="contact-method">Social</p> 
+        <div className="socials-container">
+          <span className="social-with-arrow">
+            <a className="social" href="https://www.linkedin.com/in/mayra-tellez/" target="blank">
+              LinkedIn
+            </a>
+            <span className="arrow">&#8599;</span>
+          </span>
+          <span className="social-with-arrow">
+            <a className="social" href="https://github.com/mayra-tellez" target="blank">Github</a>            
+            <span className="arrow">&#8599;</span>
+          </span>
         </div>
-        <div className="signoff-container">
-          {/* <p className="signoff">talk soon!</p> */}
-        </div>
-      </section>
+      </div>
+
+      <div className="signoff-container">
+        <p className="signoff">talk soon!</p>
+      </div>
+
       <img className="blob" src={purpleBlob} alt="purple blob"/>
+
     </div>
   );
 }
