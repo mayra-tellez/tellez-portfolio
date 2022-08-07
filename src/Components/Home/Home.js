88a916e81d9133ba371236712eec7ccb768b2328
mayrant <mayrant2004@yahoo.com>
2022-08-06 19:17:44 -0500

    home page layout and animations

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Mousewheel, Pagination } from "swiper";
@@ -16,6 +17,60 @@ import "./Home.css";
 
 const Home = () => {
 
+  useEffect(() => {
+    const greenColumn = document.getElementById("green-column");
+    const orangeColumn = document.getElementById("orange-column");
+    const purpleColumn = document.getElementById("purple-column");
+
+    const greenBlob = document.getElementById("green-blob");
+    const orangeBlob = document.getElementById("orange-blob");
+    const purpleBlob = document.getElementById("purple-blob");
+
+    const moveGreenBlob = () => {
+      greenBlob.style.position = "relative";
+      greenBlob.style.top = "140px";
+      greenBlob.style.right = "120px";
+    }
+
+    const returnGreenBlob = () => {
+      greenBlob.style.backgroundColor = "var(--background)";
+      greenBlob.style.top = "0px";
+      greenBlob.style.right = "0px";
+    }
+
+    const moveOrangeBlob = () => {
+      orangeBlob.style.position = "relative";
+      orangeBlob.style.top = "140px";
+      orangeBlob.style.right = "-100px";
+    }
+
+    const returnOrangeBlob = () => {
+      orangeBlob.style.backgroundColor = "var(--background)";
+      orangeBlob.style.top = "0px";
+      orangeBlob.style.right = "0px";
+    }
+
+    const movePurpleBlob = () => {
+      purpleBlob.style.position = "relative";
+      purpleBlob.style.top = "140px";
+      purpleBlob.style.right = "-340px";
+    }
+
+    const returnPurpleBlob = () => {
+      purpleBlob.style.backgroundColor = "var(--background)";
+      purpleBlob.style.top = "0px";
+      purpleBlob.style.right = "0px";
+    }
+
+    greenColumn.addEventListener("mouseover", moveGreenBlob);
+    orangeColumn.addEventListener("mouseover", moveOrangeBlob);
+    purpleColumn.addEventListener("mouseover", movePurpleBlob);
+
+    greenColumn.addEventListener("mouseleave", returnGreenBlob);
+    orangeColumn.addEventListener("mouseleave", returnOrangeBlob);
+    purpleColumn.addEventListener("mouseleave", returnPurpleBlob);
+  })
+
   const timeOfDay = () => {
     const today = new Date();
     const time = today.getHours();
@@ -29,63 +84,38 @@ const Home = () => {
     }
   }
 
-  const projectList = projects.projects;
-
   return (
     <div className="Home">
-      {/* <Header></Header> */}
       <main>
-        {/* <h1 className="page-title left">Hello dere</h1> */}
+        <section className="intro">
+          <p id="name">mayra téllez</p>
+          <p>Front End Software Engineer, <br /> Full Stack Trained</p>
+          <div className="blobs-container">
+            <img id="green-blob" className="blob" src={greenBlob} alt="green blob"/>
+            <img  id="orange-blob" className="blob" src={orangeBlob} alt="orange blob"/>
+            <img  id="purple-blob" className="blob" src={purpleBlob} alt="purple blob"/>
+          </div>
+        </section>
         <section className="menu-section">
-          <div id="menu-column-one">
-            <img className="blob" src={greenBlob} alt="green blob"/>
+          <div id="green-column" className="menu-column">
             <Link className="menu-link" to="/work">
               <h1 className="menu-item right">work</h1>
             </Link>
           </div>
-          <div id="menu-column-two">
-            <img className="blob" src={orangeBlob} alt="orange blob"/>
+          <div id="orange-column" className="menu-column">
             <Link className="menu-link" to="/about">
               <h1 className="menu-item center">about</h1>
             </Link>
           </div>
-          <div id="menu-column-three">
-            <img className="blob" src={purpleBlob} alt="purple blob"/>
+          <div id="purple-column" className="menu-column">
             <Link className="menu-link" to="/contact">
               <h1 className="menu-item">contact</h1>
             </Link>
           </div>
         </section>
-        {/* <>
-          <Swiper
-            direction={"vertical"}
-            slidesPerView={1}
-            mousewheel={true}
-            pagination={{
-              clickable: true,
-              // dynamicBullets: true,
-            }}
-            modules={[Mousewheel, Pagination]}
-            className="mySwiper"
-          >
-            <SwiperSlide>
-              <section id="firstSlide-home">
-                <p id="greeting">good {timeOfDay()}!</p>
-                <p id="intro">I'm Mayra Téllez <br/> & I develop the web's <br/> front end.</p>
-                <i class="fa-solid fa-angle-down"></i>
-              </section>
-            </SwiperSlide>
-            {projectList.slice(0, 2).map((project, i) => {
-              return (
-                <SwiperSlide className="featureProjectSlide" key={i}>
-                  <FeatureProjectCard project={project} key={i}></FeatureProjectCard>
-                </SwiperSlide>
-              )
-            })}
-          </Swiper>
-        </> */}
       </main>
-      {/* <Footer></Footer> */}
+
+      {/* <p>Hope you're enjoying your {timeOfDay()} <span>&#9786;</span></p> */}
     </div>
   );
 }
