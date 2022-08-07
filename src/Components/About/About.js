e678ba9759017611da19d15136ba85be0d457413
mayrant <mayrant2004@yahoo.com>
2022-08-07 14:06:28 -0500

    about paragraph

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -6,29 +6,49 @@ import orangeBlob from "../../images/orangeBlob.svg";
 import "./About.css";
 
 const About = () => {
+
+  const timeOfDay = () => {
+    const today = new Date();
+    const time = today.getHours();
+  
+    if (time >= 0 && time < 12) {
+      return "morning";
+    } else if (time >= 12 && time < 17) {
+      return "afternoon";
+    } else if (time >= 17 && time <= 23) {
+      return "evening";
+    }
+  }
+
   return (
     <div className="About">
       <Link className="navToHome-link" to="/">back to <span id="home">home</span></Link>
       <h1 className="page-title">about</h1>
       <img className="blob" src={orangeBlob} alt="orange blob"/>
       <img className="About-img" src={profilePic} alt="Mayra Tellez" />
+
       <section className="About-main">
         <h1 className="page-title">hello!</h1>
         <br />
-        <h2 className="subtitle">I'm Mayra <span>&#9786;</span> I Develop the Web's Front End</h2>
+        <h2 className="subtitle">I'm Mayra. Developer and Problem Solver.</h2>
         <p className="About-paragraph">
           <br />
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+          Nothing makes me happier than coding a solution and doing it well. As a recent graduate of Austin Coding Academy and their  internship program, I've learned there are endless possibilites to finding an answer, and that's what makes it so exciting.  
           <br />
           <br />
-          It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          I found my way to web development through some small exposure during my time in the fashion, retail and customer service industries. At Stitch Fix, I awed at the projects the engineers worked on. In my following roles at Beautifully Loved and Wildflowers Boutique, I created and updated websites first-hand. Now, I'm developing sites and apps from scratch for the people around me, and the best part of it all is that I'm helping to make a difference with work I enjoy. 
+          <br />
+          <br />
+          Feel free to browse around and let me know if there is anything I can do to help you, too. Thanks for stopping by - enjoy your {timeOfDay()}!
         </p>
         <br />
+
         <button className="resume-button">
-          <a className="resume-link" href="https://drive.google.com/file/d/16DrnpUuT90FjTkEQad46STrS7eqSbZ_7/view?export/pdf" target="blank">My Resume</a>
+          <a className="resume-link" href="https://drive.google.com/file/d/16DrnpUuT90FjTkEQad46STrS7eqSbZ_7/view?export/pdf" target="blank">My Resumé</a>
           <span className="arrow">&#8599;</span>
         </button>
       </section>
+
     </div>
   )
 }
