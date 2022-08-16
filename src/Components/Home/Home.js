ee6755fd1df14952435a7c2cc6f1607b333b8fe6
mayrant <mayrant2004@yahoo.com>
2022-08-16 17:00:08 -0500

    fixing blob movements on hover

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -20,49 +20,66 @@ const Home = () => {
     const orangeBlob = document.getElementById("orange-blob");
     const purpleBlob = document.getElementById("purple-blob");
 
+    // green stop 1 - stop-color="rgba(143.78, 178.763, 138.33, 1)"
+    // green stop 2 - stop-color="rgba(167.223, 255, 164.211, 1)"
+
+    // orange stop 1 - stop-color="rgba(255, 136.963, 43.28, 1)"
+    // orange stop 2 - stop-color="rgba(255, 225.191, 184.367, 1)"
+    
+    // purple stop 1 - stop-color="rgba(160.625, 27.318, 178.799, 1)"
+    // purple stop 2 - stop-color="rgba(249.46, 233.315, 255, 1)"
+
+    // <stop id="stop1" stop-color="rgba(143.78, 178.763, 138.33, 1)" offset="0%"></stop>
+    // <stop id="stop2" stop-color="rgba(167.223, 255, 164.211, 1)" offset="100%"></stop>
+
+    console.log(greenBlob);
+
     const moveGreenBlob = () => {
       greenBlob.style.position = "relative";
       greenBlob.style.top = "140px";
       greenBlob.style.right = "120px";
+      greenBlob.style.stopColor = "white";
     }
 
-    const returnGreenBlob = () => {
-      greenBlob.style.backgroundColor = "var(--background)";
-      greenBlob.style.top = "0px";
-      greenBlob.style.right = "0px";
-    }
+    moveGreenBlob();
 
-    const moveOrangeBlob = () => {
-      orangeBlob.style.position = "relative";
-      orangeBlob.style.top = "140px";
-      orangeBlob.style.right = "-100px";
-    }
+    // const returnGreenBlob = () => {
+    //   greenBlob.style.backgroundColor = "var(--background)";
+    //   greenBlob.style.top = "0px";
+    //   greenBlob.style.right = "0px";
+    // }
 
-    const returnOrangeBlob = () => {
-      orangeBlob.style.backgroundColor = "var(--background)";
-      orangeBlob.style.top = "0px";
-      orangeBlob.style.right = "0px";
-    }
+    // const moveOrangeBlob = () => {
+    //   orangeBlob.style.position = "relative";
+    //   orangeBlob.style.top = "140px";
+    //   orangeBlob.style.right = "-100px";
+    // }
 
-    const movePurpleBlob = () => {
-      purpleBlob.style.position = "relative";
-      purpleBlob.style.top = "140px";
-      purpleBlob.style.right = "-340px";
-    }
+    // const returnOrangeBlob = () => {
+    //   orangeBlob.style.backgroundColor = "var(--background)";
+    //   orangeBlob.style.top = "0px";
+    //   orangeBlob.style.right = "0px";
+    // }
 
-    const returnPurpleBlob = () => {
-      purpleBlob.style.backgroundColor = "var(--background)";
-      purpleBlob.style.top = "0px";
-      purpleBlob.style.right = "0px";
-    }
+    // const movePurpleBlob = () => {
+    //   purpleBlob.style.position = "relative";
+    //   purpleBlob.style.top = "140px";
+    //   purpleBlob.style.right = "-340px";
+    // }
 
-    greenColumn.addEventListener("mouseover", moveGreenBlob);
-    orangeColumn.addEventListener("mouseover", moveOrangeBlob);
-    purpleColumn.addEventListener("mouseover", movePurpleBlob);
+    // const returnPurpleBlob = () => {
+    //   purpleBlob.style.backgroundColor = "var(--background)";
+    //   purpleBlob.style.top = "0px";
+    //   purpleBlob.style.right = "0px";
+    // }
 
-    greenColumn.addEventListener("mouseleave", returnGreenBlob);
-    orangeColumn.addEventListener("mouseleave", returnOrangeBlob);
-    purpleColumn.addEventListener("mouseleave", returnPurpleBlob);
+    // greenColumn.addEventListener("mouseover", moveGreenBlob);
+    // orangeColumn.addEventListener("mouseover", moveOrangeBlob);
+    // purpleColumn.addEventListener("mouseover", movePurpleBlob);
+
+    // greenColumn.addEventListener("mouseleave", returnGreenBlob);
+    // orangeColumn.addEventListener("mouseleave", returnOrangeBlob);
+    // purpleColumn.addEventListener("mouseleave", returnPurpleBlob);
   })
 
   return (
